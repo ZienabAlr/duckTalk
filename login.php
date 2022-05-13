cb1302cdf3fa336fcc6da62efe38b970198849a6
ZienabAlr <r0784494@student.thomasmore.be>
2022-05-13 14:44:04 +0200

    login form added

--- login.php
+++ login.php
@@ -16,7 +16,53 @@
        <a href="login.php">Login</a>
        <a class="activeBtn" href="signup.php">Sign Up</a>
     </nav>
-
     </header>
+
+    <section>
+
+    <div id="signupForm">
+        <div class="wrapper">
+            <form action=""  method = "post" class="form">
+
+              <h1 class="title">Login</h1>
+                 
+              <!-- Error -->
+              <?php if (isset($error)) : ?>
+                <div class="error">
+                  <p>
+                    Sorry, we can't log you in with that email address and password. Can you try again?
+                  </p>
+                </div>
+              <?php endif; ?>
+
+                <div id="inputContainer">
+                    <input type="text" class="input" placeholder="Enter your school email adress" name="email" required="">
+                    <label for="" class="label">Email</label>
+                </div>
+
+                <div id="inputContainer">
+                    <input type="text" class="input" placeholder="a" name= "password">
+                    <label for="" class="label">Password</label>
+                </div>
+
+                <div id="inputContainer">
+                    <!-- <h4>Already have an account? -->
+                        <a href="reset-password.php" class="login">Forgot your password?</a></h4>
+                </div>
+
+
+                <input type="submit" id="submitBtn" value="Login">
+                <input type="submit" id="submitBtn" value="Beck">
+
+                <div id= "noAccount">
+                  <h4>Don't have an account? <a href="signup.php">Sign up</a></h4>
+
+                  <!-- <input type="submit" id="submitBtn" value="Signup"> -->
+                </div>
+            </form>
+        </div>
+    </div>
+
+    </section>
 </body>
 </html>
