e92bf502b7a44bfa696e217ff206cf109e6f4309
ZienabAlr <r0784494@student.thomasmore.be>
2022-06-04 17:40:30 +0200

    index edited

---
+++ inc/nav.inc.php
@@ -0,0 +1,14 @@
+<?php
+  session_start();
+?>
+<nav class="navbar">
+   
+    
+    <div class="links">
+        <!-- <a href="logout.php" class="link navbar__logout">Logout</a> -->
+        <a href="index.php" class="logo"><img src="images/logo.png" alt="ducktalk"></a>
+        <a href="#" class ="profile link"><img src="https://cdn.icon-icons.com/icons2/3138/PNG/512/profile_user_avatar_person_icon_192481.png" alt="profile"><?php /*echo $username */?> </a>
+        <!-- <a href="index.php" class= "link">Home</a> -->
+    </div>
+  
+</nav>
