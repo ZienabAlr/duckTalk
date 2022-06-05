b921833fb7dfb96022731ec0612c85a4e7cfa966
ZienabAlr <r0784494@student.thomasmore.be>
2022-06-05 13:37:24 +0200

    problem

--- index.php
+++ index.php
@@ -24,7 +24,11 @@
             <form method="post" action="" enctype='multipart/form-data'>
 
                 <div class="post">
-                    <input type="text"  class="input" name ="askQuestion" placeholder="What is your question?" value="">
+                    <h4>What is your question?</h4>
+                    <input type="text"  class="input" name ="askQuestion" placeholder="Title" value="">
+                    <input type="text" class="input wider-input" name="explain" placeholder="Write what you think..." value="">
+				    <input type="file" class="input" name ="file" value="" multiple="" >
+						
                     <!-- <label for="" class="label font">What is your question?</label> -->
                 </div>
 
