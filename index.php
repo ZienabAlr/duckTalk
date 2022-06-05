1407b395087c6af4300324b1c9fc137fb3609fe7
ZienabAlr <r0784494@student.thomasmore.be>
2022-06-05 17:34:33 +0200

    niks

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
 
-// echo "You loged in!";
 
 ?><!DOCTYPE html>
 <html lang="en">
@@ -9,7 +8,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/main.css">
-    <title>Document</title>
+    <title>DuckTalk</title>
 </head>
 <body>
     
@@ -25,22 +24,19 @@
 
                 <div class="post">
                     <h4>What is your question?</h4>
-                    <!-- <input type="text"  class="input" name ="askQuestion" placeholder="Title" value=""> 
-                    <input type="text" class="input wider-input" name="explain" placeholder="Write what you think..." value="">
-				    <input type="file" class="input" name ="file" value="" multiple="" >-->
 						
-                    <div class="inputveld">
+                    <div class="inputveld border-bottom">
                         <input type="text" class="input" name="ask" placeholder="Title" value="">
                     </div>
 
-                    <div class="inputveld ">
-                        <input type="text" class="input" name="" placeholder="Description" id="">
+                    <div class="inputveld border-bottom">
+                        <input type="text" class="input" name="" placeholder="Set description..." id="">
                     </div>
 
                     <div class="inputveld">
                         <input type="file" class="input" name="file" value="" multiple="">
                     </div>
-                    <!-- <label for="" class="label font">What is your question?</label> -->
+
                 </div>
 
             </form>
