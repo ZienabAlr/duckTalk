56e1bd8a6c8ef60a12b98e557cdfb3a410974155
ZienabAlr <r0784494@student.thomasmore.be>
2022-06-05 17:06:29 +0200

    post input

--- index.php
+++ index.php
@@ -20,15 +20,26 @@
 
     <section>
 
-        <div>
+        <div class="postform">
             <form method="post" action="" enctype='multipart/form-data'>
 
                 <div class="post">
                     <h4>What is your question?</h4>
-                    <input type="text"  class="input" name ="askQuestion" placeholder="Title" value="">
+                    <!-- <input type="text"  class="input" name ="askQuestion" placeholder="Title" value=""> 
                     <input type="text" class="input wider-input" name="explain" placeholder="Write what you think..." value="">
-				    <input type="file" class="input" name ="file" value="" multiple="" >
+				    <input type="file" class="input" name ="file" value="" multiple="" >-->
 						
+                    <div>
+                        <input type="text" class="input" name="ask" placeholder="Title" value="">
+                    </div>
+
+                    <div>
+                        <input type="text" class="input" name="" placeholder="Description" id="">
+                    </div>
+
+                    <div>
+                        <input type="file" class="input" name="file" value="" multiple="">
+                    </div>
                     <!-- <label for="" class="label font">What is your question?</label> -->
                 </div>
 
