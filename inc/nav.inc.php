27c2e1637807862167b92cf88a412f7c6efe7dd0
ZienabAlr <r0784494@student.thomasmore.be>
2022-06-14 07:41:59 +0200

    profile style added

--- inc/nav.inc.php
+++ inc/nav.inc.php
@@ -10,7 +10,7 @@
         <div class="dropdown">
           <a href="#" class ="profile link"><img src="https://cdn.icon-icons.com/icons2/3138/PNG/512/profile_user_avatar_person_icon_192481.png" alt="profile"><?php /*echo $username */?> </a>
           <div class="dropdown-content"> 
-            <a href="#">Profile</a>
+            <a href="profile.php">Profile</a>
             <a href="#">Setting</a>
             <a href="logout.php">Log Out</a>
           </div>
